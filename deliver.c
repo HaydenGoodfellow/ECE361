b0ad585d861015200c8579d707ff606db4a67d19
Hayden Goodfellow <Hayden.Goodfellow@mail.utoronto.ca>
2020-09-24 22:32:04 -0400

    Implemented deliver receiving and searching for user inputted file. Implemented error checking to ensure input is valid.  Currently checks in working directory only. Tested with textFile.txt

--- deliver.c
+++ deliver.c
@@ -1,20 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <unistd.h>
 #include <errno.h>
 #include <string.h>
+#include <dirent.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/stat.h>
 #include <netinet/in.h>
 #include <netdb.h>
 #include <arpa/inet.h>
 #include <sys/wait.h>
 #include <signal.h>
 
+#define MAX_INPUT_SIZE 128
+
+bool fileExists(char *name);
+
 int main(int argc, char **argv) {
+    // Ensure valid arguments are passed in
 	if (argc < 3 || (atoi(argv[2]) <= 0)) {
         fprintf(stderr, "Incorrect usage. Please invoke using ./deliver <Server Address> <Server Port Number>\n");
         return 0;
     }
+    char input[MAX_INPUT_SIZE];
+    bool validInput = false;
+    while (!validInput) {
+        // Recieve the file name from the user
+        printf("Please input a file name using the following format:\nftp <File Name>\n");
+        fgets(input, MAX_INPUT_SIZE, stdin);
+        // Ensures user entered "ftp " at the start of input
+        if (strncmp("ftp ", input, 4) != 0) {
+            fprintf(stderr, "Invalid input, please try again\n");
+            continue;
+        }
+        validInput = true;
+    }
+    if (!fileExists(input + 4)) { // Plus 4 to remove the "ftp "
+        fprintf(stderr, "File not found\n");
+        return 0;
+    }
 
+}
+
+// Currently assuming the file is in our current working directory
+bool fileExists(char *name) {
+    //fprintf(stderr, "Name: %s\n", name);
+    DIR *dir = opendir(".");
+    struct dirent *entry = readdir(dir);
+    while (entry != NULL) {
+        //fprintf(stderr, "File name: %s\n", entry->d_name);
+        // Using strncmp with length to avoid needing \0 at end of strings
+        if (strncmp(entry->d_name, name, strlen(entry->d_name)) == 0)
+            return true;
+        entry = readdir(dir);
+    }
+    return false;
 }
